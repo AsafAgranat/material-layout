0034a4b6649345a6cede2825a207661cd56df975
Marc Dijoux <marc.dijoux@noxu.fr>
2015-04-07 22:26:56 +0400

    Updated meteor version

--- package.js
+++ package.js
@@ -1,14 +1,14 @@
 Package.describe({
   name: 'noxu:material-layout',
-  version: '0.0.3',
+  version: '0.0.4',
   summary: "Material Design layout SASS Framework",
   git: 'https://github.com/noxu-fr/material-layout',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.0.3.2');
-  api.use("fourseven:scss@2.0.0", ["server"]);
+  api.versionsFrom('1.1.0.2');
+  api.use("fourseven:scss@2.1.1", ["server"]);
   api.imply("fourseven:scss", ["server"]);
 
   api.addFiles([
